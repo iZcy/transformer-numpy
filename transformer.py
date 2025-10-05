ae4f8ba69113898b631cce6da10ef8158c6fc48f
iZcy <izcy@users.noreply.github.com>
2025-10-05 17:32:02 +0700

    add token embedding

--- transformer.py
+++ transformer.py
@@ -1,3 +1,13 @@
 import numpy as np
 
 # basic transformer implementation
+
+class TokenEmbedding:
+    def __init__(self, vocab_size, d_model):
+        self.vocab_size = vocab_size
+        self.d_model = d_model
+        self.embedding = np.random.randn(vocab_size, d_model) * 0.01
+
+    def forward(self, x):
+        # x: (batch, seq_len)
+        return self.embedding[x]  # (batch, seq_len, d_model)
