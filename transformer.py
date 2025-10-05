4245abd532a5b93b9ff135520f1fef5026aee2b6
iZcy <izcy@users.noreply.github.com>
2025-10-05 17:32:46 +0700

    add layer norm

--- transformer.py
+++ transformer.py
@@ -37,3 +37,17 @@ def softmax(x, axis=-1):
 
 def gelu(x):
     return 0.5 * x * (1 + np.tanh(np.sqrt(2 / np.pi) * (x + 0.044715 * x**3)))
+
+
+class LayerNorm:
+    def __init__(self, d_model, eps=1e-6):
+        self.d_model = d_model
+        self.eps = eps
+        self.gamma = np.ones(d_model)
+        self.beta = np.zeros(d_model)
+
+    def forward(self, x):
+        mean = np.mean(x, axis=-1, keepdims=True)
+        var = np.var(x, axis=-1, keepdims=True)
+        x_norm = (x - mean) / np.sqrt(var + self.eps)
+        return self.gamma * x_norm + self.beta
